abad443d652b311cea41fb2a54317b5908f05726
ottsch <397865+ottsch@users.noreply.github.com>
2025-04-30 10:28:01 +0200

    Update TwigString.php

--- Loader/TwigString.php
+++ Loader/TwigString.php
@@ -9,7 +9,7 @@
 
 namespace LK\TwigstringBundle\Loader;
 
-class String extends \Twig_Loader_String
+class TwigString extends \Twig_Loader_String
 {
 	public function load($name) {
 		return new $name;
